f1b10407d84cf60ddc858a2824f064002dbd3c7a
Prajwal Tiwari <fnafchica2004@gmail.com>
2024-01-17 20:59:30 +0530

    Updated script to play BestOf5 game

--- script.js
+++ script.js
@@ -1,38 +1,65 @@
-function getComputerChoice(){
+function getComputerChoice() {
     const options = ["rock", "paper", "scissors"];
-    return options[Math.floor(Math.random()*options.length)];
+    return options[Math.floor(Math.random() * options.length)];
 }
 
-function playRound(playerSelection, ComputerSelection){
-    if(playerSelection === ComputerSelection){
-        console.log("It's a tie!!! Play Again!");
-        game();
+function game() {
+    while(numberOfRounds < 5) {
+        let playerSelection = prompt("Enter your move!");
+        let ComputerSelection = getComputerChoice();
+        console.log("Round Number: " + (numberOfRounds+1) + " Your Choice: " + playerSelection + " Computer Choice: " + ComputerSelection);
+
+        playRound(playerSelection.toLowerCase(), ComputerSelection);
     }
-    else if(playerSelection == "rock"){
-        if(ComputerSelection == "scissors")
-            console.log("You Win!!! :D Rock defeats Scissors!");
-        else
-            console.log("You Lose :\"( Rock loses to Paper");
+    function playRound(playerSelection, ComputerSelection) {
+        if (playerSelection === ComputerSelection) {
+            console.log("It's a tie!!! Play Again!");
+            game();
+        }
+        else if (playerSelection == "rock") {
+            if (ComputerSelection == "scissors"){
+                console.log("You Win!!! :D Rock defeats Scissors!");
+                playerwins++;
+            }
+            else{
+                console.log("You Lose :\"( Rock loses to Paper");
+                computerwins++;
+            }
+        }
+        else if (playerSelection == "paper") {
+            if (ComputerSelection == "rock"){
+                console.log("You Win!!! :D Paper defeats Rock!");
+                playerwins++;
+            }
+            else{
+                console.log("You Lose :\"( Paper loses to Scissors");
+                computerwins++;
+            }
+        }
+        else if (playerSelection == "scissors") {
+            if (ComputerSelection == "paper"){
+                console.log("You Win!!! :D Scissors defeat Paper!");
+                playerwins++;
+            }
+            else{
+                console.log("You Lose :\"( Scissors lose to Rock");
+                computerwins++;
+            }
+        }
+        numberOfRounds++;
+
     }
-    else if(playerSelection == "paper"){
-        if(ComputerSelection == "rock")
-            console.log("You Win!!! :D Paper defeats Rock!");
-        else
-            console.log("You Lose :\"( Paper loses to Scissors");
-    }
-    else if(playerSelection == "scissors"){
-        if(ComputerSelection == "paper")
-            console.log("You Win!!! :D Scissors defeat Paper!");
-        else
-            console.log("You Lose :\"( Scissors lose to Rock");
-    }
-    
 }
 
-function game(){
-    let playerSelection = prompt("Enter your move!");
-    let ComputerSelection = getComputerChoice();
-    console.log("Your Choice: " + playerSelection + " Computer Choice: " + ComputerSelection);
-    playRound(playerSelection.toLowerCase(), ComputerSelection);
-}
+let playerwins = 0;
+let computerwins = 0;
+let numberOfRounds = 0;
+console.log("Welcome to a Best-of-Five Rock, Paper, Scissors game.");
 game();
+
+if(playerwins > computerwins){
+    console.log("Congratulations! You have Won the game!!! " + "You: " + playerwins + " Computer: " + computerwins);
+}
+else{
+    console.log("Oops You Lost the Game :( " + "You: " + playerwins + " Computer: " + computerwins);
+}
