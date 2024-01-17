b72bb1d8e2e23269e4bd8b1ae1bacf781b25026b
Prajwal Tiwari <fnafchica2004@gmail.com>
2024-01-17 20:44:51 +0530

    Update script.js
    Added playRound, game functions to play single round game.

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 function getComputerChoice(){
-    const options = ["rock", "paper", "scissor"];
+    const options = ["rock", "paper", "scissors"];
     return options[Math.floor(Math.random()*options.length)];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, ComputerSelection){
+    if(playerSelection === ComputerSelection){
+        console.log("It's a tie!!! Play Again!");
+        game();
+    }
+    else if(playerSelection == "rock"){
+        if(ComputerSelection == "scissors")
+            console.log("You Win!!! :D Rock defeats Scissors!");
+        else
+            console.log("You Lose :\"( Rock loses to Paper");
+    }
+    else if(playerSelection == "paper"){
+        if(ComputerSelection == "rock")
+            console.log("You Win!!! :D Paper defeats Rock!");
+        else
+            console.log("You Lose :\"( Paper loses to Scissors");
+    }
+    else if(playerSelection == "scissors"){
+        if(ComputerSelection == "paper")
+            console.log("You Win!!! :D Scissors defeat Paper!");
+        else
+            console.log("You Lose :\"( Scissors lose to Rock");
+    }
+    
+}
+
+function game(){
+    let playerSelection = prompt("Enter your move!");
+    let ComputerSelection = getComputerChoice();
+    console.log("Your Choice: " + playerSelection + " Computer Choice: " + ComputerSelection);
+    playRound(playerSelection.toLowerCase(), ComputerSelection);
+}
+game();
